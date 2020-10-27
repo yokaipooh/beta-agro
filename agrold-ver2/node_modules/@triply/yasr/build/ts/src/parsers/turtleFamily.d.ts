3fa00e19e0f9bccf5970bc3de40f2d5aa3a5e511
yokaipooh <asddsse4@gmail.com>
2020-10-28 01:10:15 +0700

    á

---
+++ agrold-ver2/node_modules/@triply/yasr/build/ts/src/parsers/turtleFamily.d.ts
@@ -0,0 +1,2 @@
+import Parser from "./";
+export default function (queryResponse: any): Parser.SparqlResults;
---
+++ agrold-ver2/static/config/config.js
@@ -0,0 +1,5 @@
+WEBAPPURL="http://127.0.0.1:5000/";
+SPARQLENDPOINTURL="http://agrold.southgreen.fr/sparql";
+FACETEDURL="http://agrold.southgreen.fr/fct";
+AGROLDAPIJSONURL=WEBAPPURL + "/api/webservices";
+DEFAULTAPIFORMAT = ".jsonld";
